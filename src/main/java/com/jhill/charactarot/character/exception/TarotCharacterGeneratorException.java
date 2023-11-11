bef83fcf28696d2460bc65344f873c6ba30a10b3
thejaxonhill <thejaxonhill@gmail.com>
2023-11-11 09:37:43 -0700

    Added TarotCharacterGeneratorException

---
+++ src/main/java/com/jhill/charactarot/character/exception/TarotCharacterGeneratorException.java
@@ -0,0 +1,7 @@
+package com.jhill.charactarot.character.exception;
+
+public class TarotCharacterGeneratorException extends RuntimeException {
+    public TarotCharacterGeneratorException(String message) {
+        super(message);
+    }
+}
